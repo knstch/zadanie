9eb6b2afd697dc49871e1489dea2b5489d188f2a
Konstantin <kostyacherepanov1@gmail.com>
2020-08-14 16:02:26 +0700

    Dobavil obrabotchiki sobitii na knopki

--- src/components/names-list-items/names-list-items.js
+++ src/components/names-list-items/names-list-items.js
@@ -1,47 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const axios = require('axios');
+export default class NamesListItems extends Component {
+    constructor (props) {
+        super(props);
+    this.onDelete = this.onDelete.bind(this);
+    this.onEdit = this.onEdit.bind(this);
+    };
 
-class DynamicList {
-    
-    constructor(FirstName, LastName) {
-        this.FirstName = FirstName;
-        this.LastName = LastName;
+    onDelete() {
+        console.log('del');
     }
+
+    onEdit() {
+        console.log('edit')
+    }
+
     render() {
         return (
-        <li>
-            <span>{this.FirstName}</span>
-            <span>{this.LastName}</span>
-            <div>
-                <button>Удалить</button>
-                <button>Редактировать</button>
-            </div>
-        </li>
+            <li className='round'>
+                <span>Сергей</span>
+                <span>Иванов</span>
+                <div>
+                    <button onClick={this.onDelete}>Удалить</button>
+                    <button onClick={this.onEdit}>Редактировать</button>
+                </div>
+            </li>
         )
     }
-}
-
-
-NamesListItems = () => {
-    axios.get('http://localhost:3000/names')
-    .then(data => {
-        data.data.forEach(({FirstName, LastName}) => {
-            new DynamicList (FirstName, LastName).render();
-        });
-    })
-}
-// const NamesListItems = () => {
-//     return (
-        // <li className='round'>
-        //     <span>Сергей</span>
-        //     <span>Иванов</span>
-        //     <div>
-        //         <button>Удалить</button>
-        //         <button>Редактировать</button>
-        //     </div>
-        // </li>
-//     )
-// }
-
-export default NamesListItems;
+};

4168a48d87bfb12c44b135a9e057f710180d5994
Konstantin <kostyacherepanov1@gmail.com>
2020-08-17 17:06:43 +0700

    modified:   db.json 	modified:   src/components/app/app.js 	modified:   src/components/names-list-add-window/names-list-add-window.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import NamesList from '../names-list';
-import AddWindow from '../names-list-add-window';
 import AddModal from '../names-list-add-modal'
 import './app.css';
 
--- src/components/names-list-add-window/names-list-add-window.js
+++ src/components/names-list-add-window/names-list-add-window.js
@@ -1,49 +1,6 @@
-import React, {useState, Component} from 'react'
-import { Button, Modal, Form, ButtonGroup } from 'react-bootstrap';
+import React, { Component } from 'react'
+import { Button, Form } from 'react-bootstrap';
 const axios = require('axios')
-
-// const AddWindow = () => {
-//     const [show, setShow] = useState(false);
-
-//     const handleClose = () => setShow(false);
-//     const handleShow = () => setShow(true);
-
-//     return (
-//       <>
-//         <Button variant="primary" onClick={handleShow}>
-//           Добавить пользователя
-//         </Button>
-  
-//         <Modal show={show} onHide={handleClose}>
-//           <Modal.Header closeButton>
-//             <Modal.Title>Новый сотрудник</Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <Form>
-//                 <Form.Group controlId="formBasicFirstName">
-//                     <Form.Label>Имя</Form.Label>
-//                     <Form.Control type="firstName"/>
-//                 </Form.Group>
-//                 <Form.Group controlId="formBasicLastName">
-//                     <Form.Label>Фамилия</Form.Label>
-//                     <Form.Control type="lastName" />
-//                 </Form.Group>
-//                 <Modal.Footer>
-//                   <Button variant="secondary" onClick={handleClose}>
-//                     Закрыть
-//                   </Button>
-//                   <Button variant="primary" onClick={handleClose}>
-//                     Добавить
-//                   </Button>
-//                 </Modal.Footer>
-//             </Form>
-//           </Modal.Body>
-//             </Modal>
-//       </>
-//     );
-// };
-
-// export default AddWindow;
 
 export default class AddWindow extends Component {
   state = {
